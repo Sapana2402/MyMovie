be9c721f0b3293c767324dd2e0d7c853c442cfcf
Sapana2402 <sapna.prajapati2001@gmail.com>
2025-08-18 17:07:37 +0530

    Initial Commit

---
+++ MyMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMovie
+//
+//  Created by MACM26 on 18/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMovie/MyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieApp.swift
+//  MyMovie
+//
+//  Created by MACM26 on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
